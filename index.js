5ce68d4afa076542149106cdea573558460e0b6d
Ismail910 <ismailmagdy566@gmail.com>
2023-03-07 20:53:41 +0200

    marg

--- index.js
+++ index.js
@@ -14,33 +14,24 @@ const bookRouter = require("./routes/book");
 const bookUserRouter = require("./routes/bookUser");
 const userRegisterRouter = require("./routes/userRegister");
 const userLoginRouter= require("./routes/userLogin");
-<<<<<<< HEAD
 const popularRouter= require("./routes/Popular");
 
-
-//const reviewsRouter = require("./routes/reviews");
-
-const categoryRouter=require("./routes/Category");
-
-const homeUserRouter = require("./routes/homeUser")
-=======
 const reviewsRouter = require("./routes/reviews");
 const categoryRouter=require("./routes/Category");
 const homeUserRouter = require("./routes/homeUser");
->>>>>>> e0e72d64b7ca539dc17a7a5cb19fd53a19f86b6a
+
 
 ////////////////Routers
 app.use(['/book' , '/books'], bookRouter);
 app.use('/admin/author',authorRouter);
-<<<<<<< HEAD
+
 
 app.use('/popular' , popularRouter);
 
 app.use('/category' , categoryRouter);
 
-=======
+
 app.use('/category' , categoryRouter);
->>>>>>> e0e72d64b7ca539dc17a7a5cb19fd53a19f86b6a
 app.use('/bookUser' , bookUserRouter);
 app.use('/register' , userRegisterRouter);
 app.use('/login' , userLoginRouter);
@@ -48,13 +39,9 @@ app.use('/reviews' , reviewsRouter);
 app.use('/home',homeUserRouter);
 
 
-<<<<<<< HEAD
-app.use('/home',homeUserRouter);
 
 
 
-=======
->>>>>>> e0e72d64b7ca539dc17a7a5cb19fd53a19f86b6a
 //middleware auth
 const auth = require("./middlewares/auth");
 app.post("/welcome", auth, (req, res) => {
