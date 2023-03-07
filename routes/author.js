534558f3a11e687567824ad303f46d8d05b326c1
IbrahimSalahEldin <ibrahimsalah824@gmail.com>
2023-03-07 04:58:09 +0200

    author updates

--- routes/author.js
+++ routes/author.js
@@ -33,9 +33,10 @@ router.get('/',async(req,res)=>{
 router.get('/:id',async(req,res)=>{
     const id=req.params.id;
     try{
-     const author= await authorModel.find({ID:id},{ID:1,photo:1,firstName:1,lastName:1,
+   //   const author= await authorModel.find({ID:id},{ID:1,photo:1,firstName:1,lastName:1,
 
-                                                dateOfBirth:1,books:1});
+   //                                              dateOfBirth:1,books:1,});
+   const author= await authorModel.find({_id:id}).populate('book');
       return res.json(author);
    }
    catch(err){
