1ded87ec714ca54074c463cefd06b3607277fd0d
IbrahimSalahEldin <ibrahimsalah824@gmail.com>
2023-03-07 03:49:37 +0200

    bookReview

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,4 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const reviewModel = require('../model/books/Reviews')
 
+
+//get all reviews
+router.get('/', async(req , res)=>{
+    try{
+        const reviews= await reviewModel.find({});
+        return res.json(reviews);
+    }catch(err){
+        res.status(404).send(err);
+    }
+})
+
+//get review by id
+
+router.get('/:id', async (req,res)=>{
+    try{
+        const review = await reviewModel.findById({_id:req.params.id});
+        return res.json(review);
+    }
+    catch(err){
+        res.status(404).send(err);
+    }
+})
+
+
+//create review
+
+router.post('/',async(req,res)=>{
+    try{
+        const review = await reviewModel.create(req.body);
+        return res.status(200).json(review);
+    }
+    catch(err){
+        return res.status(500).send(err);
+    }
+})
+
+
+
+//update review
+router.put('/',async(req,res)=>{
+    try{
+        const review = await reviewModel.updateOne({_id:req.params.id},{$set:req.body});
+        return res.json(review);
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+})
+
+
+module.exports = router;
 
 
 
