20773b405f3885c941634ccd40f39faa8327a9d6
Ismail910 <ismailmagdy566@gmail.com>
2023-03-19 20:49:44 +0200

    marg

--- routes/homeUser.js
+++ routes/homeUser.js
@@ -28,14 +28,15 @@ router.get("/all/page/:page/:userID",authUser,async(req,res)=>{
       const limit=5;
       const bookCount=await bookModel.find({}).count();
       const totalPages=Math.ceil(bookCount/limit);
-     const books = await bookUserModel 
+     const books = await bookUserModel
      .find({user:req.params.userID},{status:1,rating:1,book:1})
      .populate({path:'book',model:'book',select:{id:1,img: 1, name: 1,avg_rate:1,summary:1}
          ,populate:{
              path:'author',
              model:'author',
              select:{'firstName':1,'lastName':1,"_id":0}
-         }})
+             
+    }})
      .limit(limit).skip((page-1)*limit).exec();
 
      const objBooks=
@@ -63,7 +64,15 @@ router.get("/all/page/:page",async(req,res)=>{
       const limit=5;
       const bookCount=await bookModel.find({}).count();
       const totalPages=Math.ceil(bookCount/limit);
-     const books = await bookModel.find({},{img:1,name:1,avg_rate:1,summary:1,id:1})
+      const books = await bookUserModel
+      .find({},{status:1,rating:1,book:1})
+      .populate({path:'book',model:'book',select:{id:1,img: 1, name: 1,avg_rate:1,summary:1}
+      ,populate:{
+          path:'author',
+          model:'author',
+          select:{'firstName':1,'lastName':1,"_id":0}}
+ })
+    //  const books = await bookModel.find({},{img:1,name:1,avg_rate:1,summary:1,id:1})
      .limit(limit).skip((page-1)*limit).exec();
 
      const objBooks=
@@ -75,6 +84,7 @@ router.get("/all/page/:page",async(req,res)=>{
         },
         data:books
      };
+     console.log(books);
       return res.json(objBooks);
     } catch (error) {
         return res.status(500).send(error);
