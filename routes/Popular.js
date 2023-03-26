e22c8fb7fe6339b66d7fcd2af62ddfb05874bb7a
OmarGamal07 <omar.gamal.dev@gmail.com>
2023-03-27 01:12:53 +0200

    editting popular category

--- routes/Popular.js
+++ routes/Popular.js
@@ -77,18 +77,40 @@ router.get('/popularAuthor', async (req,res)=>{
 router.get('/popularCategory', async (req,res)=>{
     try{
        
-        const popularBook = await bookModel.find({},{'avg_rate':1,"category":1})
-        .sort({avg_rate: -1}).limit(5)
-        // .populate({path:'category', select: {'name':1, _id:0}})
-        //const popularCategory = await CategoryModel.find({_id:},{})
-         const data = popularBook.forEach(async (ele )=>{
-            await CategoryModel.find({id:ele.category},{})
+        // const popularBook = await bookModel.find({},{'avg_rate':1,"category":1})
+        // .sort({avg_rate: -1}).limit(5)
+        // // .populate({path:'category', select: {'name':1, _id:0}})
+        // //const popularCategory = await CategoryModel.find({_id:},{})
+        //  const data = popularBook.forEach(async (ele )=>{
+        //     await CategoryModel.find({id:ele.category},{})
+        // })
+        // console.log(data);
+
+        // console.log(popularBook);
+
+        // return res.json({"sortAuthor":"jjj"});
+        const popularCategories = await bookModel.aggregate([
+          {
+            $group: {
+              _id: "$category",
+              rate: { $sum: avg_rate }
+            }
+          },
+          {
+            $sort: { rate: -1 }
+          },
+          {
+            $limit: 5
+          }
+        ]);
+        let respopularCategories = await Promise.all(  //wait to store data in array 
+        popularCategories.map(async (element) => {
+          let res=element.rate;
+          res.category=CategoryModel.find({_id: element._id}, {name: 1});
+          return res;
         })
-        console.log(data);
-
-        console.log(popularBook);
-
-        return res.json({"sortAuthor":"jjj"});
+      );
+      return res.json(respopularCategories);
     }catch(err){
         res.status(500).send(err);
        }
