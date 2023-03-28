d115ba798b38ac183b870319b9812de3d607dfe3
IbrahimSalahEldin <ibrahimsalah824@gmail.com>
2023-03-28 03:12:17 +0200

    merg

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 const mongoose = require('mongoose');
 
 var cors = require('cors');
-const PORT= process.env.Port || 5000 ;
+const PORT=  5000 ;
 
 
 
@@ -54,12 +54,6 @@ app.use('/home',homeUserRouter);
 
 
 
-
-//middleware auth
-// const auth = require("./middlewares/auth");
-// app.post("/welcome", auth, (req, res) => {
-//   res.status(200).send("Welcome 🙌 ");
-// });
 
 //end middleware auth
 
